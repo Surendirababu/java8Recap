632ab96798860e7f8599ba38cfb7030fd4a7cdd7
surendirababu <babusuren@hotmail.com>
2018-02-01 22:25:57 +0530

    worked on the before and after methods of current date

--- src/main/java/java8/LocalDateJava8.java
+++ src/main/java/java8/LocalDateJava8.java
@@ -1,6 +1,7 @@
 package java8;
 
 import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
 import java.time.temporal.TemporalField;
 
 /**
@@ -11,14 +12,63 @@ public class LocalDateJava8 {
     public static void main(String a[])
     {
 
-        LocalDate localDate = LocalDate.now();
+        LocalDate now = LocalDate.now();
 
-        System.out.println("Day of Month : "+localDate.getDayOfMonth());
-        System.out.println("Month : "+localDate.getMonth());
-        System.out.println("Month Value : "+localDate.getMonthValue());
-        System.out.println("Day of Week : "+localDate.getDayOfWeek());
-        System.out.println("Day of year : "+localDate.getDayOfYear());
-        System.out.println("Era : "+localDate.getEra());
+        System.out.println("Day of Month : "+now.getDayOfMonth());
+        System.out.println("Month : "+now.getMonth());
+        System.out.println("Month Value : "+now.getMonthValue());
+        System.out.println("Day of Week : "+now.getDayOfWeek());
+        System.out.println("Day of year : "+now.getDayOfYear());
+        System.out.println("Era : "+now.getEra());
+
+
+        System.out.println("------------------------------");
+        //Ten Days After Current Day
+        LocalDate tenDaysAfterCurrentDate = now.plusDays(10);
+        System.out.println("Day of Month : "+tenDaysAfterCurrentDate.getDayOfMonth());
+        System.out.println("Month : "+tenDaysAfterCurrentDate.getMonth());
+
+        System.out.println("------------------------------");
+
+        //Ten Days Before Current Day
+        LocalDate tenDaysBeforeCurrentDate = now.minusDays(10);
+        System.out.println("Day of Month : "+tenDaysBeforeCurrentDate.getDayOfMonth());
+        System.out.println("Month : "+tenDaysBeforeCurrentDate.getMonth());
+
+        System.out.println("------------------------------");
+
+        System.out.println("Is before : "+now.isBefore(tenDaysBeforeCurrentDate));
+        System.out.println("Is after : "+now.isAfter(tenDaysBeforeCurrentDate));
+
+        System.out.println("------------------------------");
+
+        System.out.println("Is before : "+now.isBefore(tenDaysAfterCurrentDate));
+        System.out.println("Is after : "+now.isAfter(tenDaysAfterCurrentDate));
+        /*
+
+        OUTPUT:
+        -------
+
+        Day of Month : 1
+        Month : FEBRUARY
+        Month Value : 2
+        Day of Week : THURSDAY
+        Day of year : 32
+        Era : CE
+        ------------------------------
+        Day of Month : 11
+        Month : FEBRUARY
+        ------------------------------
+        Day of Month : 22
+        Month : JANUARY
+        ------------------------------
+        Is before : false
+        Is after : true
+        ------------------------------
+        Is before : true
+        Is after : false
+
+        */
 
 
     }
